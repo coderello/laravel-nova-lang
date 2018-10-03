5e4305ef2b2aaa5f8dd14a8417c52eadbfcc798a
DowayneB <dowayne.b@afrihost.com>
2018-10-03 13:43:46 +0200

    Update validation.php
    Added Afrikaans and Zulu translations

--- resources/lang/za/validation.php
+++ resources/lang/za/validation.php
@@ -1,1 +1,15 @@
-
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'attached' => 'Hierdie: Attribuut is reeds aangeheg.'."\r\n".'Lokhu: imfanelo isivele ifakwe.',
+    'relatable' => 'Hierdie: kenmerk mag nie met hierdie hulpbron geassosieer word nie.'."\r\n".'Lokhu: imfanelo ingase ingahlotshaniswa nale nsiza.',
+];
