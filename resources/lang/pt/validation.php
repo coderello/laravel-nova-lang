dc40da4df8bbd21c9753d6a8c58cafd6be202b33
pedro <pedrokraktuga@gmail.com>
2018-10-04 11:13:00 +0100

    Validation.php Translated to Portuguese

---
+++ resources/lang/pt/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Este :attribute já foi anexado.',
+    'relatable' => 'Este :attribute não pode ser associado com este recurso.',
+
+];
