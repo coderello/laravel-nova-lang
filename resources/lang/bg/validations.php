09d8423d2cf5aee8ca8b292f94a8bbffb600d5d4
Bozhidar Kirev <nekyvmail@mail.bg>
2018-10-09 15:33:34 +0300

    Added translations for BG

---
+++ resources/lang/bg/validations.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'attached' => 'Полето :attribute вече е прикпрепено.',
+    'relatable' => 'Полето :attribute не може да бъде асоциирано с този ресурс.',
+];
