3b100c1c03c978b32594ab777126aedd00be0e7a
Joe Bordes <joe@tsolucio.com>
2018-10-27 21:52:26 +0200

    i18n(ca)

---
+++ resources/lang/ca/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Aquest :attribute ja ha estat adjuntat.',
+    'relatable' => 'Aquest :attribute pot ser que no estigui associat a aquest recurs.',
+
+];
