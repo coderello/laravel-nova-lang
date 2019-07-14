c599571761140e19c0d4ac0909e7bb608b6f91d5
Kit Burton-Senior <mail@kitbs.com>
2019-07-14 18:29:17 +0100

    add ability to import contributors from github

--- src/Commands/NovaLangStats.php
+++ src/Commands/NovaLangStats.php
@@ -52,29 +52,30 @@ class NovaLangStats extends Command
 
             return;
         }
-        
+
         $sourceDirectory = $this->directoryNovaSource().'/en';
         $sourceFile = $sourceDirectory.'.json';
-        
+
         if (!$this->filesystem->exists($sourceDirectory) || !$this->filesystem->exists($sourceFile)) {
             $this->error('The source language files were not found in the vendor/laravel/nova directory.');
 
             return;
         }
-        
+
         $outputDirectory = storage_path('app/nova-lang');
         $this->filesystem->makeDirectory($outputDirectory, 0777, true, true);
-        
+
         $contributorsFile = __DIR__.'/../../contributors.json';
         $contributors = collect(json_decode($this->filesystem->get($contributorsFile), true));
-                        
+
         $sourceKeys = array_keys(json_decode($this->filesystem->get($sourceFile), true));
         $sourceCount = count($sourceKeys);
-        
+
         $availableLocales = $this->getAvailableLocales();
-        
-        $availableLocales->each(function (string $locale) use ($contributors, $sourceKeys, $sourceCount) {
-            
+        $blame = collect($this->getBlame());
+
+        $availableLocales->each(function (string $locale) use ($contributors, $sourceKeys, $sourceCount, $blame) {
+
             $inputDirectory = $this->directoryFrom().'/'.$locale;
 
             $inputFile = $inputDirectory.'.json';
@@ -84,14 +85,16 @@ class NovaLangStats extends Command
             $missingKeys = array_diff($sourceKeys, $localeKeys);
 
             $localeStat = $contributors->get($locale, [
-                'name' => class_exists('Locale') ? Locale::getDisplayName($locale) : $locale,
+                'name' => class_exists('Locale') ? \Locale::getDisplayName($locale) : $locale,
                 'contributors' => [],
             ]);
-            
+
+            if ($blameContributors = $blame->get($locale)) {
+                $localeStat['contributors'] = $blameContributors;
+            }
+
             $localeStat['complete'] = $sourceCount - count($missingKeys);
-            
-            // TODO: Count contributions using git blame?
-            
+
             $localeStat['contributors'] = collect($localeStat['contributors'])
                 ->map(function($lines, $name) {
                     return compact('lines', 'name');
@@ -99,40 +102,40 @@ class NovaLangStats extends Command
                 return $a['lines'] === $b['lines'] ? $a['name'] <=> $b['name'] : 0 - ($a['lines'] <=> $b['lines']);
             })->map(function($contributor) {
                 return $contributor['lines'];
-            })->all();
-            
+            })->keys()->all();
+
             $contributors->put($locale, $localeStat);
 
         });
-        
+
         $contributors = $contributors->sort(function($a, $b) {
             return $a['complete'] === $b['complete'] ? $a['name'] <=> $b['name'] : 0 - ($a['complete'] <=> $b['complete']);
         });
-        
+
         $outputFile = $outputDirectory.'/contributors.json';
-        
+
         $this->filesystem->put($outputFile, json_encode($contributors, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-        
+
         $this->info(sprintf('Updated "contributors.json" has been output to [%s].', $outputFile));
         $this->warn('* Replace contributors.json in your fork of the repository with this file.');
-        
+
         $contributors->transform(function($localeStat, $locale) use ($sourceCount) {
-            
+
             $percent = round(($localeStat['complete'] / $sourceCount) * 100, 1).'%';
-            
+
             $contributors = implode(', ', array_map(function($contributor) {
                 return sprintf('[%s](https://github.com/%s)', $contributor, $contributor);
             }, array_keys($localeStat['contributors'])));
-            
+
             return sprintf('| %s | [%s](resources/lang/%s.json) | %d (%s) | %s |', $localeStat['name'], $locale, $locale, $localeStat['complete'], $percent, $contributors);
         });
-                
+
         $outputFile = $outputDirectory.'/README.excerpt.md';
-        
+
         $header = '## Available Languages'.PHP_EOL.PHP_EOL.'| Language | Code | Lines translated | Thanks to |'.PHP_EOL.'| --- | --- | --- | --- |';
-        
+
         $this->filesystem->put($outputFile, $header.PHP_EOL.$contributors->join(PHP_EOL));
-        
+
         $this->info(sprintf('Updated "README.excerpt.md" has been output to [%s].', $outputFile));
         $this->warn('* Replace the Available Languages table in README.md in your fork of the repository with the contents of this file.');
     }
@@ -160,5 +163,65 @@ class NovaLangStats extends Command
     protected function directoryNovaSource(): string
     {
         return base_path('vendor/laravel/nova/resources/lang');
+    }
+
+    protected function getBlame(): array
+    {
+        $token = env('GITHUB_TOKEN_NOVALANG');
+
+        $contributions = ['en' => ['taylorotwell' => 10001, 'bonzai' => 10000, 'davidhemphill' => 10000, 'themsaid' => 10000]];
+
+        if (!$token) {
+            return $contributions;
+        }
+
+        $graphql = 'query { repository(owner: "coderello", name: "laravel-nova-lang") { pullRequests(first: 100, states: [MERGED]) { nodes { number title body state merged changedFiles files(first: 100) { nodes { path additions deletions } } author { login } } } } }';
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://api.github.com/graphql',
+            CURLOPT_USERAGENT => 'langcompare',
+            CURLOPT_HTTPHEADER => [
+                'Authorization: bearer '.$token,
+                'Content-Type: application/json',
+            ],
+            CURLOPT_POST => 1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode(['query' => $graphql]),
+        ]);
+
+        $result = json_decode(curl_exec($curl), true);
+
+        curl_close($curl);
+
+        if (!isset($result['data']['repository']['pullRequests']['nodes'])) {
+            return $contributions;
+        }
+
+        $pullRequests = $result['data']['repository']['pullRequests']['nodes'];
+
+        foreach ($pullRequests as $pullRequest) {
+            if (!in_array($pullRequest['number'], [148, 156], true)) {
+                $author = $pullRequest['author']['login'] ?? '(deleted)';
+
+                foreach ($pullRequest['files']['nodes'] as $file) {
+                    if (stripos($file['path'], 'resources/lang') === 0) {
+                        $locale = preg_replace('@resources/lang/([^./]+)[./].*@', '$1', $file['path']);
+
+                        if ($locale != 'cn' && $file['additions']) {
+                            if (isset($contributions[$locale][$author])) {
+                                $contributions[$locale][$author] += $file['additions'];
+                            }
+                            else {
+                                $contributions[$locale][$author] = $file['additions'];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $contributions;
     }
 }
