cc153cf8d1928889c743b8810ddc4f0d70246433
Bernard van Wyngaard <bernard.wyngaard@afrihost.com>
2019-10-12 13:00:23 +0200

    Added Afrikaans validation

---
+++ resources/lang/af/validation.php
@@ -0,0 +1,20 @@
+<?php
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Hierdie :attribute is reeds aangeheg.',
+    'relatable' => 'Hierdie :attribute mag nie met hierdie hulpbron geassosieer word nie.',
+
+];
