96213fd23da15c87daf2f05244dbf583c89feddc
soulshockers <soulshockers@gmail.com>
2019-10-13 22:43:43 +0300

    Ukrainian language updated (#186)

--- resources/lang/uk/validation.php
+++ resources/lang/uk/validation.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'attached' => 'This :attribute is already attached.',
-    'relatable' => 'This :attribute may not be associated with this resource.',
+    'attached' => 'Поле :attribute вже прикріплено.',
+    'relatable' => 'Поле :attribute не може бути повязано із цим ресурсом.',
 
 ];
