73ae6cc68436972a9b62070eddfe0cc1000b3670
Hajrudin <hajroahemtasevic92@gmail.com>
2019-10-15 00:38:50 +0200

    Added Bosnian translation (#188)

---
+++ resources/lang/bs/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Ovaj :attribute je vec zakacen.',
+    'relatable' => 'Ovaj :attribute mozda nije povezan sa ovim resursom.',
+
+];
