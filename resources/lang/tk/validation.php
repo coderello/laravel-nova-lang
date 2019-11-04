d2bf5cb2e52847356f5c6d1a284f09447ca10087
Kakajan SH <mervasdayi@gmail.com>
2019-11-04 15:20:25 +0500

    Added Turkmen translations (#198)

---
+++ resources/lang/tk/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Bu :attribute eýýäm baglanan.',
+    'relatable' => 'Bu :attribute häzirki maglumat bilen baglanşykly bolman biler.',
+
+];
