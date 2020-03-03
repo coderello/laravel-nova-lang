6de5ec6ae84f377a90af7495033dffdd48268d2b
shokhaa <muxawa@gmail.com>
2020-03-03 18:07:27 +0500

    uz latin 80% done

---
+++ resources/lang/uz-Latin/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Qiymatlarni tasdiqlash uchun til manbalari
+    |--------------------------------------------------------------------------
+    |
+    | Quydagi til qatorlarida ishatilgan standart xabarlar mavjud :attribute maydon nomi(validator).
+    | Bazi bir qoidlarni bir necha xil ko`rinishlari mavjud! Maslan: o`lcham.
+    | Siz ularni hohlagan paytingizda ilovangizga mos ravishda o`zgartrishingiz mumkin
+    */
+
+    'attached' => ':attribute maydoni allaqachon biriktirilgan.',
+    'relatable' => 'Bu :attribute maydonini ushbu resurs bilan bog`lab bo`lmaydi.',
+
+];
