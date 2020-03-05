7d396be0172a6f610f6bd4d639707d085771bcd4
Kit Burton-Senior <mail@kitbs.com>
2020-03-05 16:26:25 +0000

    add --formal option to publish

--- src/Commands/NovaLangPublish.php
+++ src/Commands/NovaLangPublish.php
@@ -25,6 +25,7 @@ class NovaLangPublish extends Command
                             {--all : Publish all languages}
                             {--alias= : Publish files using a different filename for certain locales, in the format "locale:alias,..."}
                             {--U|underscore : Use underscore instead of dash as locale separator }
+                            {--formal= : Publish the formal variants of certain locales, comma-separated}
                             {--force : Override existing files}';
 
     /**
@@ -58,16 +59,23 @@ class NovaLangPublish extends Command
      */
     public function handle()
     {
+        if ($this->isFormalLocale($this->argument('locales')) || $this->isFormalLocale($this->option('alias'))) {
+            $this->error('You must not specify the @formal suffix in the locales argument or --alias option.');
+            return;
+        }
+
         $availableLocales = $this->getAvailableLocales();
 
         $requestedLocales = $this->getRequestedLocales();
+
+        $formalLocales = $this->getFormalLocales();
 
         if (!$requestedLocales->count()) {
             $this->error('You must either specify one or more locales, or use the --all option.');
             return;
         }
 
-        $requestedLocales->each(function (string $alias, string $locale) use ($availableLocales) {
+        $requestedLocales->each(function (string $alias, string $locale) use ($availableLocales, $formalLocales) {
 
             if ($alias == 'en' && $this->isForce()) {
                 if (!$this->confirm(sprintf('Are you sure you want to publish translations for [en] locale? This will overwrite the file from laravel/nova.'))) {
@@ -113,6 +121,23 @@ class NovaLangPublish extends Command
 
             $this->filesystem->copy($inputFile, $outputFile);
 
+            if ($formalLocales->contains($locale)) {
+                $inputFileFormal = $inputDirectory . '@formal.json';
+
+                if ($this->filesystem->exists($inputFileFormal)) {
+                    $formalKeys = json_decode($this->filesystem->get($inputFileFormal), true);
+                    $informalKeys = json_decode($this->filesystem->get($inputFile), true);
+
+                    $outputKeys = array_merge($informalKeys, $formalKeys);
+
+                    $this->filesystem->put($outputFile, json_encode($outputKeys, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+
+                    return $this->info(sprintf('<fg=green;options=bold>Formal</> translations for [%s] locale have been published successfully.', $locale, $asAlias));
+                }
+
+                $this->warn(sprintf('There is no formal variant for [%s] locale. The ordinary translation file will be published instead.', $locale, $asAlias));
+            }
+
             $this->info(sprintf('Translations for [%s] locale have been published successfully%s.', $locale, $asAlias));
         });
     }
@@ -122,9 +147,12 @@ class NovaLangPublish extends Command
         if ($this->isAll()) {
             $locales = $this->getAvailableLocales();
         }
-        else {
+        elseif ($this->argument('locales')) {
             $locales = $this->fixSeparators($this->argument('locales'));
             $locales = collect(explode(',', $locales))->filter();
+        }
+        else {
+            return collect();
         }
 
         $aliases = $this->getLocaleAliases($locales->count() == 1 ? $locales->first() : false);
@@ -146,6 +174,22 @@ class NovaLangPublish extends Command
         return $locales;
     }
 
+    protected function getFormalLocales(): Collection
+    {
+        if (!$this->option('formal')) {
+            return collect();
+        }
+
+        if ($this->isAllFormal()) {
+            $locales = $this->getAvailableFormalLocales();
+        } else {
+            $locales = $this->fixSeparators($this->option('formal'));
+            $locales = collect(explode(',', $locales))->filter();
+        }
+
+        return $locales;
+    }
+
     protected function getAvailableLocales(): Collection
     {
         $localesByDirectories = collect($this->filesystem->directories($this->directoryFrom()))
@@ -156,9 +200,23 @@ class NovaLangPublish extends Command
         $localesByFiles = collect($this->filesystem->files($this->directoryFrom()))
             ->map(function (SplFileInfo $splFileInfo) {
                 return str_replace('.'.$splFileInfo->getExtension(), '', $splFileInfo->getFilename());
+            })->filter(function ($locale) {
+                return !$this->isFormalLocale($locale);
             });
 
         return $localesByDirectories->intersect($localesByFiles)->values();
+    }
+
+    protected function getAvailableFormalLocales(): Collection
+    {
+        return collect($this->filesystem->files($this->directoryFrom()))
+            ->map(function (SplFileInfo $splFileInfo) {
+                return str_replace('.'.$splFileInfo->getExtension(), '', $splFileInfo->getFilename());
+            })->filter(function ($locale) {
+                return $this->isFormalLocale($locale);
+            })->map(function ($locale) {
+                return $this->stripFormalLocale($locale);
+            })->values();
     }
 
     protected function getLocaleAliases($single = false): Collection
@@ -211,9 +269,19 @@ class NovaLangPublish extends Command
         return $aliases;
     }
 
-    protected function fixSeparators(string $locale, string $separator = '-')
+    protected function fixSeparators(?string $locale, string $separator = '-'): string
     {
         return preg_replace('/['.static::SEPARATORS.']+/', $separator, $locale);
+    }
+
+    public function isFormalLocale(?string $locale): bool
+    {
+        return stripos($locale, '@formal') !== false;
+    }
+
+    public function stripFormalLocale(?string $locale): string
+    {
+        return str_replace('@formal', '', $locale);
     }
 
     protected function isForce(): bool
@@ -224,6 +292,11 @@ class NovaLangPublish extends Command
     protected function isAll(): bool
     {
         return $this->option('all');
+    }
+
+    protected function isAllFormal(): bool
+    {
+        return $this->option('formal') == '*';
     }
 
     protected function directoryFrom(): string
