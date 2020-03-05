716a58e24cfa96b29559b40266c2fd002a0e138f
Kit Burton-Senior <kitbs@users.noreply.github.com>
2020-03-05 11:56:36 +0000

    Replace ` with turned comma

--- resources/lang/uz-Latn/validation.php
+++ resources/lang/uz-Latn/validation.php
@@ -8,11 +8,11 @@ return [
     |--------------------------------------------------------------------------
     |
     | Quydagi til qatorlarida ishatilgan standart xabarlar mavjud :attribute maydon nomi(validator).
-    | Bazi bir qoidlarni bir necha xil ko`rinishlari mavjud! Maslan: o`lcham.
-    | Siz ularni hohlagan paytingizda ilovangizga mos ravishda o`zgartrishingiz mumkin
+    | Bazi bir qoidlarni bir necha xil koʻrinishlari mavjud! Maslan: oʻlcham.
+    | Siz ularni hohlagan paytingizda ilovangizga mos ravishda oʻzgartrishingiz mumkin
     */
 
     'attached' => ':attribute maydoni allaqachon biriktirilgan.',
-    'relatable' => 'Bu :attribute maydonini ushbu resurs bilan bog`lab bo`lmaydi.',
+    'relatable' => 'Bu :attribute maydonini ushbu resurs bilan bogʻlab boʻlmaydi.',
 
 ];
