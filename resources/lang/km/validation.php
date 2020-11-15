b20282c2d0a4d361ac816c5531fe99ecd2bdd052
Chhaihong SRUN <chhaihongsrun@gmail.com>
2020-11-15 18:31:14 +0700

    Adding Khmer language

---
+++ resources/lang/km/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => ':attribute នេះត្រូវបានភ្ជាប់រួចហើយ។',
+    'relatable' => ':attribute នេះប្រហែលជាមិនមានទំនាក់ទំនងជាមួយធនធាននេះទេ។',
+
+];
